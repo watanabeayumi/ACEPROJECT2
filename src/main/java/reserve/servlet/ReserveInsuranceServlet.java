20d845d3136f798bfbf900179e11306af22ed940
yuuri <yuuri.sb@gmail.com>
2023-06-13 11:02:17 +0900

    ReserveInsuranceServlet 修正

--- src/main/java/reserve/servlet/ReserveInsuranceServlet.java
+++ src/main/java/reserve/servlet/ReserveInsuranceServlet.java
@@ -1,6 +1,9 @@
 package reserve.servlet;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,11 +11,28 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import reserve.dao.DaoException;
+import reserve.dao.ReserveDAO;
+import reserve.dto.Reserve;
+import reserve.flowbean.ReserveCalendarFlowBean;
+
 @WebServlet("/reserveInsurance")
 public class ReserveInsuranceServlet extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		Date strDate = new Date();
+		LocalDate nowDate = strDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+		
+		try {
+			Reserve reserve = null;
+			reserve = new ReserveDAO().selectReserve(nowDate, 1, 4);
+			
+			ReserveCalendarFlowBean flowbean = new ReserveCalendarFlowBean;
+			flowbean
+		} catch (DaoException e) {
+			e.printStackTrace();
+		}
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		

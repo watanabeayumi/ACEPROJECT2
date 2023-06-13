62e9aba06b817b3572f1fb5376c8c53901374171
risa <19e2132@u.seijo.ac.jp>
2023-06-13 16:05:03 +0900

    j

---
+++ src/main/java/reserve/flowbean/SearchFlowBean.java
@@ -0,0 +1,26 @@
+package reserve.flowbean;
+
+public class SearchFlowBean {
+	String name;
+	String call;
+	String mail;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCall() {
+		return call;
+	}
+	public void setCall(String call) {
+		this.call = call;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+}
--- src/main/java/reserve/servlet/SearchServlet.java
+++ src/main/java/reserve/servlet/SearchServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import reserve.formbean.SearchFormBean;
+
 
 @WebServlet("/search")
 public class SearchServlet extends HttpServlet {
@@ -16,7 +18,15 @@ public class SearchServlet extends HttpServlet {
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+
+		SearchFormBean formBean=new SearchFormBean();
 		
+		request.getAttribute("errMsg",errMsg);
+		
+		if(!errMsg.isEmpty()) {
+			request.getRequestDispatcher("/confirm.jsp").forward(request, response);
+			
+		}
 		
 		
 	}

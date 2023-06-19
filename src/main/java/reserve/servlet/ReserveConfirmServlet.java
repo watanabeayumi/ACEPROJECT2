b258dbadb6657d736b2ded4fe32ffba8ee74aae3
yuuri <yuuri.sb@gmail.com>
2023-06-19 10:44:30 +0900

    karennda-

--- src/main/java/reserve/servlet/ReserveConfirmServlet.java
+++ src/main/java/reserve/servlet/ReserveConfirmServlet.java
@@ -50,53 +50,27 @@ public class ReserveConfirmServlet extends HttpServlet {
 		
 		int reserveDate = formbean.getReserveDate();
 		
-		if(reserveDate%7==1) {
-			flowbean.setReserve_date(nowDate.plusDays(1));
-		}else if(reserveDate%7==2) {
-			flowbean.setReserve_date(nowDate.plusDays(2));
-		}else if(reserveDate%7==3) {
-			flowbean.setReserve_date(nowDate.plusDays(3));
-		}else if(reserveDate%7==4) {
-			flowbean.setReserve_date(nowDate.plusDays(4));
-		}else if(reserveDate%7==5) {
-			flowbean.setReserve_date(nowDate.plusDays(5));
-		}else if(reserveDate%7==6) {
-			flowbean.setReserve_date(nowDate.plusDays(6));
-		}else if(reserveDate%7==0) {
-			flowbean.setReserve_date(nowDate.plusDays(7));
+		for(int i=0; i<=6; i++) {
+			if(i==0) {
+				if(reserveDate%7==0) {
+					flowbean.setReserve_date(nowDate.plusDays(7));
+					break;
+				}
+			}else {
+				if(reserveDate%7==i) {
+					flowbean.setReserve_date(nowDate.plusDays(i));
+					break;
+				}
+			}
 		}
 		
 		//4.予約時間の表示設定。
-		if(reserveDate>=1&&reserveDate<=7) {
-			flowbean.setTimeCd(1);
-			flowbean.setTimeName("10:00∼11:00");
-		}else if(reserveDate>=8&&reserveDate<=14) {
-			flowbean.setTimeCd(2);
-			flowbean.setTimeName("11:00∼12:00");
-		}else if(reserveDate>=15&&reserveDate<=21) {
-			flowbean.setTimeCd(3);
-			flowbean.setTimeName("12:00∼13:00");
-		}else if(reserveDate>=22&&reserveDate<=28) {
-			flowbean.setTimeCd(4);
-			flowbean.setTimeName("13:00∼14:00");
-		}else if(reserveDate>=29&&reserveDate<=35) {
-			flowbean.setTimeCd(5);
-			flowbean.setTimeName("14:00∼15:00");
-		}else if(reserveDate>=36&&reserveDate<=42) {
-			flowbean.setTimeCd(6);
-			flowbean.setTimeName("15:00∼16:00");
-		}else if(reserveDate>=43&&reserveDate<=49) {
-			flowbean.setTimeCd(7);
-			flowbean.setTimeName("16:00∼17:00");
-		}else if(reserveDate>=50&&reserveDate<=56) {
-			flowbean.setTimeCd(8);
-			flowbean.setTimeName("17:00∼18:00");
-		}else if(reserveDate>=57&&reserveDate<=63) {
-			flowbean.setTimeCd(9);
-			flowbean.setTimeName("18:00∼19:00");
-		}else if(reserveDate>=64&&reserveDate<=70) {
-			flowbean.setTimeCd(10);
-			flowbean.setTimeName("19:00∼20:00");
+		for(int i=0; i<=9; i++) {
+			if(reserveDate>=(1+7*i)&&reserveDate<=(7+7*i)) {
+				flowbean.setTimeCd(i+1);
+				flowbean.setTimeName((i+10) + ":00∼" + i+11 + ":00");
+				break;
+			}
 		}
 		
 		//5.予約した値を、flowbeanのcall,mail,nameにセットする。

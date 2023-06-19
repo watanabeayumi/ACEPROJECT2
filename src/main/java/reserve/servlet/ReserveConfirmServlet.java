0a37bfb935b82fd71802838a529c7e5c521c0f02
yuuri <yuuri.sb@gmail.com>
2023-06-19 17:26:36 +0900

    ca

--- src/main/java/reserve/servlet/ReserveConfirmServlet.java
+++ src/main/java/reserve/servlet/ReserveConfirmServlet.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpSession;
 
 import reserve.dao.DaoException;
 import reserve.dao.ReserveDAO;
+import reserve.dto.Reserve;
 import reserve.flowbean.ReserveSearchFlowBean;
 import reserve.formbean.ReserveSearchFormBean;
 
@@ -41,12 +42,41 @@ public class ReserveConfirmServlet extends HttpServlet {
 		List<String> mailErr = formbean.checkMail(request);
 		
 		if(!reserveErr.isEmpty() || !nameErr.isEmpty() || !callErr.isEmpty() || !mailErr.isEmpty()){
-			request.setAttribute("ReserveErr", reserveErr);
-			request.setAttribute("NameErr", nameErr);
-			request.setAttribute("CallErr", callErr);
-			request.setAttribute("MailErr", mailErr);
+			if(!reserveErr.isEmpty()) {
+				request.setAttribute("ReserveErr", reserveErr);
+			}
+			if(!nameErr.isEmpty()) {
+				request.setAttribute("NameErr", nameErr);
+			}
+			if(!callErr.isEmpty()) {
+				request.setAttribute("CallErr", callErr);
+			}
+			if(!mailErr.isEmpty()) {
+				request.setAttribute("MailErr", mailErr);
+			}
 			request.getRequestDispatcher("/WEB-INF/jsp/reserve/search.jsp").forward(request, response);
 			return;
+		}
+		
+		ReserveDAO dao = new ReserveDAO();
+		try {
+			Reserve reserve = dao.reserve(formbean.getName(), formbean.getCall(), formbean.getMail());
+			if(reserve != null) {
+				String conciergeName = dao.concierge(reserve.getConciergeCd());
+				String timeName = dao.time(reserve.getTimeCd());
+				
+				flowbean.setName(formbean.getName());
+				flowbean.setCall(formbean.getCall());
+				flowbean.setMail(formbean.getMail());
+				flowbean.setReserveDate(reserve.getReserveDate());
+				flowbean.setConciergeName(conciergeName);
+				flowbean.setTimeName(timeName);
+				
+				session.setAttribute("AlreadyReserve", flowbean);
+				request.getRequestDispatcher("/WEB-INF/jsp/reserve/alreadyReserve.jsp").forward(request, response);
+			}
+		} catch (DaoException e) {
+			e.printStackTrace();
 		}
 		
 		LocalDate nowDate = LocalDate.now();
@@ -62,7 +92,6 @@ public class ReserveConfirmServlet extends HttpServlet {
 		}
 		
 		//4.選択した予約カレンダーから選択した時間帯を特定する
-		ReserveDAO dao = new ReserveDAO();
 		for(int i=0; i<=9; i++) {
 			if(reserveDate>=(1+7*i)&&reserveDate<=(7+7*i)) {
 				flowbean.setTimeCd(i+1);

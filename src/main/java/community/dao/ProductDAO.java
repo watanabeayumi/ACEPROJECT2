8d478cb73728059464f3541f977f2d4a51108474
yuuri <yuuri.sb@gmail.com>
2023-06-23 16:52:16 +0900

    fyiy

--- src/main/java/community/dao/ProductDAO.java
+++ src/main/java/community/dao/ProductDAO.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import community.dto.Product;
 import reserve.dao.CommonDAO;
 import reserve.dao.DaoException;
 
@@ -13,9 +14,9 @@ public class ProductDAO extends CommonDAO {
 	
 	private static final String SELECT_REVIEW_JAN = "SELECT * FROM p_review WHERE jan_code=?";
 	
-	public List<String> selectJan(int jan_code) throws DaoException{
+	public List<Product> selectJan(int jan_code) throws DaoException{
 		
-		List<String> reviewList = new ArrayList<>();
+		List<Product> reviewList = new ArrayList<>();
 		
 		getConnection();
 		try {
@@ -24,7 +25,16 @@ public class ProductDAO extends CommonDAO {
 			
 			ResultSet resultSet = statement.executeQuery();
 			while(resultSet.next()) {
-				
+				Product product = new Product();
+				product.setUserName(resultSet.getString("user_name"));
+				product.setProductName(resultSet.getString("product_name"));
+				product.setStar(resultSet.getInt("star"));
+				product.setJanCd(resultSet.getInt("jan_code"));
+				product.setCategory(resultSet.getString("category"));
+				product.setGenre(resultSet.getString("genre"));
+				product.setBland(resultSet.getString("bland"));
+				product.setReview(resultSet.getString("review"));
+				reviewList.add(product);
 			}
 		} catch (SQLException e) {
 			// TODO 自動生成された catch ブロック
--- src/main/java/community/servlet/ReviewUproad.java
+++ src/main/java/community/servlet/ReviewUproad.java
@@ -1,6 +1,7 @@
 package community.servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,7 +10,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import community.dao.ProductDAO;
+import community.dto.Product;
 import community.flowbean.ProductFlowBean;
+import reserve.dao.DaoException;
 
 
 @WebServlet("/productReview")
@@ -19,10 +23,22 @@ public class ReviewUproad extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		request.setCharacterEncoding("utf-8");
+		
 		HttpSession session=request.getSession(false);
 		ProductFlowBean flowbean = (ProductFlowBean)session.getAttribute("Product");
-		if(flowbean.getJanCd()!=null) {
-			request.getRequestDispatcher("/WEB-INF/jsp/product/productreview.jsp").forward(request, response);
+		String janCd = flowbean.getJanCd();
+		if(janCd!=null) {
+			int jan_code = Integer.parseInt(janCd);
+			try {
+				List<Product> reviewList = new ProductDAO().selectJan(jan_code);
+				
+				request.setAttribute("ReviewList", reviewList);
+				request.getRequestDispatcher("/WEB-INF/jsp/product/productreview.jsp").forward(request, response);
+			} catch (DaoException e) {
+				// TODO 自動生成された catch ブロック
+				e.printStackTrace();
+			}
 		}
 		request.getRequestDispatcher("/WEB-INF/jsp/product/productreview.jsp").forward(request, response);
 	}

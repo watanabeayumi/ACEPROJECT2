d9447261dfe648c88a19317806c75e3f37cf6837
risa <19e2132@u.seijo.ac.jp>
2023-06-23 13:40:31 +0900

    a

--- src/main/java/community/servlet/ReviewUproad.java
+++ src/main/java/community/servlet/ReviewUproad.java
@@ -7,18 +7,19 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
-@WebServlet("/productReview")
-public class ProductReviewServlet extends HttpServlet {
+@WebServlet("/reviewUproad")
+public class ReviewUproad extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		
-		request.getRequestDispatcher("/WEB-INF/jsp/product/review.jsp").forward(request, response);
+		HttpSession session=request.getSession(false);
+		request.getRequestDispatcher("/WEB-INF/jsp/product/reviewUproad.jsp").forward(request, response);
 	}
 
 }
